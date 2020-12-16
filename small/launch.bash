d324e9998dda9de46c40fc1faf2b3762c842db82
Ernesto Gutierrez <ernesto.dev2@gmail.com>
2020-12-15 22:30:42 -0500

    Images are now smaller in size.  Use of only two conky windows for the full setup

--- small/launch.bash
+++ small/launch.bash
@@ -4,16 +4,5 @@ killall conky
 sleep 5
 
 directory=~/scripts/conky/monochrome/small
-conky -c ${directory}/cpu &
-conky -c ${directory}/memory &
-conky -c ${directory}/wifi &
-conky -c ${directory}/network-up &
-conky -c ${directory}/network-down &
-conky -c ${directory}/diskio-write &
-conky -c ${directory}/diskio-read &
-conky -c ${directory}/power &
-conky -c ${directory}/cpuProcesses &
-conky -c ${directory}/memoryProcesses &
-conky -c ${directory}/diskSpace &
-conky -c ${directory}/usbStick &
-conky -c ${directory}/usbDrive &
+conky -c ${directory}/leftPanel &
+conky -c ${directory}/rightPanel &
