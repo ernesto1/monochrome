d0df3af6c1f30e9a77069114c8c4bf2a231a0bc6
Ernesto Gutierrez <ernesto.dev2@gmail.com>
2021-08-18 11:52:21 -0400

    Fixed load average data point extraction for desktops running for longer than 24 hours

--- dnfPackageLookup.bash
+++ dnfPackageLookup.bash
@@ -8,7 +8,13 @@ halfCores=$(( totalCores / 2 ))
 echo "$(date +'%D %r') - system is deemed iddle if the 5 min cpu load average is less than ${halfCores}" | tee /tmp/conkyDnf.log
 
 while [ true ]; do
-    loadAvg=$(uptime | cut -d , -f 4)       # get the 5 min load average
+    # the output format of `uptime` changes if the machine runs for longer than a day
+    #
+    #  within a day
+    #                                                                      1m    5m    15m
+    #  after a day:     22:54:03 up 2 days,  2:12,  1 user,  load average: 0.53, 1.16, 1.40
+    loadAvg=$(uptime)
+    loadAvg=$(echo ${loadAvg#*load average: } | cut -d, -f2)
     echo "$(date +'%D %r') - 5 min load avg = $loadAvg" | tee -a /tmp/conkyDnf.log
     
     # perform dnf lookup if the system is iddle
