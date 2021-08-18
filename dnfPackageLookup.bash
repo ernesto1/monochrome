57b0b28b7aa2a089418b56da572a095446a2d4f2
Ernesto Gutierrez <ernesto.dev2@gmail.com>
2021-08-18 12:22:21 -0400

    missing comment

--- dnfPackageLookup.bash
+++ dnfPackageLookup.bash
@@ -9,10 +9,9 @@ echo "$(date +'%D %r') - system is deemed iddle if the 5 min cpu load average is
 
 while [ true ]; do
     # the output format of `uptime` changes if the machine runs for longer than a day
-    #
-    #  within a day
-    #                                                                      1m    5m    15m
-    #  after a day:     22:54:03 up 2 days,  2:12,  1 user,  load average: 0.53, 1.16, 1.40
+    #                                                               1m    5m    15m
+    #  within a day:    12:20:31 up 37 min,  1 user,  load average: 0.86, 0.73, 0.66
+    #  more than a day: 22:54:03 up 2 days,  2:12,  1 user,  load average: 0.53, 1.16, 1.40
     loadAvg=$(uptime)
     loadAvg=$(echo ${loadAvg#*load average: } | cut -d, -f2)
     echo "$(date +'%D %r') - 5 min load avg = $loadAvg" | tee -a /tmp/conkyDnf.log
