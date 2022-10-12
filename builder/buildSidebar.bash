312bdce752da9917957ab15b0203261f516a661d
Ernesto Gutierrez <ernesto.dev2@gmail.com>
2022-10-12 14:54:36 -0400

    conky source files updated to support laptop setup

--- builder/buildSidebar.bash
+++ builder/buildSidebar.bash
@@ -25,20 +25,20 @@ do
     image=${imageCommand#*image }   # remove '${image '
     image=${image% -n *}            # remove ' -n -p 0,489}\'
     imagePath=$(echo "echo $image" | bash)
-    echo $imagePath    
+    echo image: $imagePath    
     coordinates=$(echo ${imageCommand} | grep -Eo '[[:digit:]]+,-*[[:digit:]]+')
-    echo $coordinates
-    yCoordinate=${coordinates#*,}
-    echo yCoordinate: $yCoordinate
-    ((newYCoordinate=yCoordinate+totalHeight))
-    echo new y coordinate: $newYCoordinate
+    echo coordinates: $coordinates
     xCoordinate=${coordinates%,*}
     echo x: $xCoordinate
+    yCoordinate=${coordinates#*,}
+    echo y: $yCoordinate
+    ((newYCoordinate=yCoordinate+totalHeight))
+    echo new y: $newYCoordinate    
     sed -i "s#${image} -n -p ${xCoordinate},${yCoordinate}#${image} -n -p ${xCoordinate},${newYCoordinate}#" ~/conky/monochrome/widgets-dock/sidebar
     echo
   done
   
-  echo using last image as reference for block height
+  echo '> using last image as reference for block height'
   dimensions=$(file "${imagePath}" | grep -Eo '[[:digit:]]+ *x *[[:digit:]]+')
   echo dimensions: $dimensions
   height=${dimensions#* x }
