0d0fc9106e0d9012fa971c8b42ba4a498f43824e
Ernesto Gutierrez <ernesto.dev2@gmail.com>
2022-12-27 16:14:17 -0500

    add freemarker template configuration creator app

---
+++ builder/freemarker-configuration/src/main/java/com/conky/ConkyTemplate.java
@@ -0,0 +1,135 @@
+package com.conky;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class ConkyTemplate {
+    private static final Logger logger = LoggerFactory.getLogger(ConkyTemplate.class);
+    private static final String CONKY_DIR = "/home/ernesto/conky/monochrome";
+    /**
+     * Root directory with the configuration files for all themes
+     */
+    private static final String CONKY_BUILD_DIR = CONKY_DIR + "/builder";
+    private static final String OUTPUT_DIR = "/tmp/monochrome/target";
+
+    /**
+     * usage: conkyTemplate <conky theme> <color>
+     * @param args
+     * @throws IOException
+     * @throws TemplateException
+     */
+    public static void main(String[] args) throws IOException, TemplateException {
+        validateArguments(args);
+
+        // load global data model
+        File buildDirectory = new File(CONKY_BUILD_DIR);
+        InputStream globalSettingsStream = new FileInputStream(new File(buildDirectory, "globalSettings.yml"));
+        Yaml yaml = new Yaml();
+        Map<String, Object> root = yaml.load(globalSettingsStream);
+        // load conky theme data model
+        File templateDirectory = new File(buildDirectory, args[0]);
+        InputStream colorPaletteStream = new FileInputStream(new File(templateDirectory, "colorPalette.yml"));
+        Map<String, Object> themes = yaml.load(colorPaletteStream);
+        // select desired color
+        String color = args[1];
+
+        if (themes.containsKey(color)) {
+            logger.info("using {} color scheme", color);
+            root.putAll((Map<String, Object>) themes.get(color));
+            logger.debug("global + theme data model: {}", root);
+        } else {
+            logger.error("color scheme '{}' is not configured for this conky, available colors are {}", color, themes.keySet());
+            System.exit(1);
+        }
+
+        Configuration cfg = createFreemarkerConfiguration(templateDirectory);
+        File outputDirectory = new File(OUTPUT_DIR);
+
+        if (!outputDirectory.exists()) {
+            // turns out the create directory operation "fails" if the directory already exists
+            boolean isDirectoryCreated = outputDirectory.mkdirs();
+
+            if (!isDirectoryCreated) {
+                logger.error("unable to create the output directory {}", OUTPUT_DIR);
+                System.exit(1);
+            }
+        }
+
+        logger.info("processing template files:");
+        // merging templates and the data model
+        for (File templateFile : templateDirectory.listFiles((d, f) -> f.endsWith(".ftl"))) {
+            logger.info("> {}", templateFile.getName());
+            Template template = cfg.getTemplate(templateFile.getName());
+            int dotPosition = templateFile.getName().lastIndexOf('.');
+            Writer out = new FileWriter(new File(outputDirectory, templateFile.getName().substring(0, dotPosition)));
+            template.process(root, out);
+            out.close();
+        }
+    }
+
+    private static void validateArguments(String[] args) {
+        if (args.length !=2) {
+            System.err.println("usage: conkyTemplate <conky theme> <color>");
+            System.exit(1);
+        }
+
+        // verify conky theme directory exists
+        Path path = Paths.get(CONKY_DIR, args[0]);
+
+        if (Files.isDirectory(path)) {
+            logger.info("creating configuration files for the {} conky", args[0]);
+        } else {
+            logger.error("conky directory {} does not exist", path);
+            System.exit(1);
+        }
+    }
+
+    private static Configuration createFreemarkerConfiguration(File templateDirectory) throws IOException {
+        // Create your Configuration instance, and specify if up to what FreeMarker
+        // version (here 2.3.29) do you want to apply the fixes that are not 100%
+        // backward-compatible. See the Configuration JavaDoc for details.
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+
+        // Specify the source where the template files come from. Here I set a
+        // plain directory for it, but non-file-system sources are possible too:
+        cfg.setDirectoryForTemplateLoading(templateDirectory);
+
+        // From here we will set the settings recommended for new projects. These
+        // aren't the defaults for backward compatibilty.
+
+        // Set the preferred charset template files are stored in. UTF-8 is
+        // a good choice in most applications:
+        cfg.setDefaultEncoding("UTF-8");
+
+        // change interpolation syntax from ${x} to [=x]
+        // this is because conky uses ${..} for its variables
+        cfg.setInterpolationSyntax(Configuration.SQUARE_BRACKET_INTERPOLATION_SYNTAX);
+
+        // Sets how errors will appear.
+        // During web page *development* TemplateExceptionHandler.HTML_DEBUG_HANDLER is better.
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        // Don't log exceptions inside FreeMarker that it will thrown at you anyway:
+        cfg.setLogTemplateExceptions(false);
+
+        // Wrap unchecked exceptions thrown during template processing into TemplateException-s:
+        cfg.setWrapUncheckedExceptions(true);
+
+        // Do not fall back to higher scopes when reading a null loop variable:
+        cfg.setFallbackOnNullLoopVariable(false);
+        return cfg;
+    }
+}
