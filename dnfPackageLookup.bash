2b584e2295799083fed1ef46b2bbc9129f46b610
Ernesto Gutierrez <ernesto.dev2@gmail.com>
2023-04-09 11:31:53 -0400

    dynamically compute the column width of new packages
    update dnf branding for widgets and grape widgets-dock
    themes

--- dnfPackageLookup.bash
+++ dnfPackageLookup.bash
@@ -6,8 +6,8 @@
 # conky variables are introduced in order for the data to be properly formatted for conky to display
 
 function usage {
-  echo $(basename $0) [--width n] >&2
-  echo 'where width is the number of characters per row, default is 35'
+  echo $(basename $0) [--width n]
+  echo 'where width is the maximun number of characters per row, default is 35'
 }
 
 function onExitSignal {
@@ -39,7 +39,12 @@ while (( "$#" )); do
   esac
 done
 
-scriptName=$(basename $0 .bash)
+if [[ $width -lt 20 ]]; then
+  echo 'width should not be less than 20 characters, not much meaningfull info can be displayed othwerise' >&2
+  exit 1
+fi
+
+width=$(( width - 6 - 1 ))      # column width for package name
 outputDir=/tmp/conky
 mkdir -p ${outputDir} 
 logFile=${outputDir}/dnf.log
@@ -80,12 +85,13 @@ while [ true ]; do
             # extract the actual packages from the raw dnf data
             grep -E $regex ${packagesRawFile} > ${packagesFile}
             # formatting done to the new package list for conky to display it nicely
-            # - package name and version is converted into a tabular layout with the given max width of characters
+            # - package name and version is converted into a tabular layout
             # - a ${voffset} is added for the text to not appear "squished"
             # - an ${offset} is added to each line in order for the package list to be printed with a left border
             # - packages of interest are surrounded by a ${color} variable in order to have them highlighted
             highlightRegex='kernel\|firefox\|transmission'
-            cat ${packagesFile} | awk '{ printf "%-23.23s %6.6s\n", $1, $2 }' | sed 's/^/${voffset 2}${offset 5}/' \
+            cat ${packagesFile} | awk "{ printf \"%-${width}.${width}s %6.6s\n\", \$1, \$2 }" \
+            | sed 's/^/${voffset 2}${offset 5}/' \
             | sed "s:\($highlightRegex\):$\{color2\}\1$\{color\}:" > ${outputDir}/dnf.packages.formatted
         else
             echo 'no updates available' | tee -a ${logFile}
@@ -96,5 +102,5 @@ while [ true ]; do
     fi
     
     sleep 5m &   # run the sleep process in the background so we can kill it if we get a terminate signal
-    wait          # wait for the sleep process to complete
+    wait         # wait for the sleep process to complete
 done
