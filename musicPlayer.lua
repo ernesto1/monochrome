281c9d5aae8ae90c70a254dfa7996ab383a9342e
Ernesto Gutierrez <ernesto.dev2@gmail.com>
2023-05-07 19:39:00 -0400

    add ability to restrict the number of characters printed
    the read_file() lua function now has an optional
    argument to restrict the number of characters printed
    
    this allows a conky to trim a file being read,
    ensuring the desired window width can be maintained

--- musicPlayer.lua
+++ musicPlayer.lua
@@ -1,7 +1,8 @@
 -- :::::: lua functions for the media player conky ::::::
 
---[[ generates an image conky variable from a conky expression, ex. the image path is in a file
-that must be read; you can use this method to extract said path by using the conky ${cat} variable
+--[[ generates an image conky variable from a conky expression which yields an image file path, 
+ex. if the image path is in a file that must be read; you can use this method to extract said path
+by using the conky ${cat} variable
 
 arguments:
   expression    conky variable to parse, must yield an image file path
@@ -17,12 +18,18 @@ function conky_album_art_image(expression, dimensions, position)
   return s
 end
 
---[[ wrapper function meant for the ${cat someFile} conky variable
-     if the file does not exist, it will print a default error message]]
-function conky_read_file(expression)
+--[[ wrapper function to complement the ${cat someFile} conky variable
+prints the contents of the file but if the file does not exist it will print a default error message
+
+arguments:
+  expression    conky 'cat' variable to parse
+  charLimit     [optional] maximun number of characters to print, use to truncate text if too long
+]]
+function conky_read_file(expression, charLimit)
+  charLimit = tonumber(charLimit) or 1000
   local text = conky_parse(expression)
   -- if the expression evaluates to nothing use a default error message
   text = (text ~= '') and text or "missing input file"
   
-  return text
+  return string.sub(text,1,charLimit)
 end
