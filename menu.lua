267e5a5bcb6f71c3f87d321cd94a0bdcd92ac8ac
Ernesto Gutierrez <ernesto.dev2@gmail.com>
2023-05-22 15:57:32 -0400

    improve documentation

--- menu.lua
+++ menu.lua
@@ -1,4 +1,22 @@
--- :::::: functions for drawing menus with dynamic sizes ::::::
+--[[  :::::: functions for drawing menus with dynamic sizes ::::::
+
+all these methods allow a conky client to manipulate x,y positions
+for images or text dynamically at runtime, examples:
+
+1) drawing menu windows
+
+      +----------+    a menu window is drawn by conky using a combination
+      |----------|    of several images imposed on top of each other
+      |          |
+      |          |    
+      |          |    for menu's with a variable amount of text
+      +----------+  < the position of the bottom edge image is determined at runtime based
+                      on the amount of text/lines in the body of the menu
+
+2) draw an image taking into account a x,y offsets to alter the image's original x,y coordinates
+3) alter text ${goto x} or ${offset x} statements with a runtime x offset
+
+]]
 
 --[[ table to hold parsed conky variables ]]
 computations = {}
@@ -17,14 +35,68 @@ function conky_compute(expression)
 end
 
 --[[ parses the given conky expression and stores its value in the 'computations' table for future use
+within the conky cycle
+
+this will allow you to run an expensive command once and then use its output multiple times within a session
 
 arguments:
-      key           key to store the expression as
-      expression    conky variable to parse
+    key           key to store the expression as
+    expression    conky variable to parse
 ]]
 function conky_compute_and_save(key, expression)
   computations[key] = conky_parse(expression)
   return computations[key]
+end
+
+--[[ prints the menu's bottom edge ${image} variable based on the number of lines in the body
+
+arguments:
+    theme     conky theme: compact, widets, glass, etc...
+    filename  image file name
+    x         image x coordinate
+    y         starting y coordinate of the menu's body image
+    voffset   vertical offset used for each line in the menu's body
+              ie. if the text uses this notation
+              
+                      ${voffset 3} some text
+                      ${voffset 3} bla bla bla
+                                 \
+                                 you have to provide this value here
+                                 
+    lines     number of lines in the body
+]]
+function conky_bottom_edge(theme, filename, x, y, voffset, lines)
+  local lineMultiplier = 15
+  
+  if tonumber(voffset) > 2 then
+    lineMultiplier = lineMultiplier + tonumber(voffset) - 2
+  end
+
+  lines = (lines > 0) and lines or 0
+  y = tonumber(y) + (lines * lineMultiplier) - 1
+  local path = "~/conky/monochrome/images/" .. theme .. "/" .. filename
+  local s = build_image_variable(path, x, y)
+  -- add a blank image right below the bottom edge image, assume bottom edges will never be greater than 15px
+  s = s .. "${image ~/conky/monochrome/images/menu-blank.png -p " .. x .. "," .. y + 15 .. "}"
+
+  return s
+end
+
+--[[ wrapper method for the 'bottom_edge' function
+allows the client to provide a conky expression/variable that when computed/parsed will return the number of lines in the body of the menu
+
+arguments:
+    ...         same arguments as conky_bottom_edge(..)
+    expression  conky expression to evaluate, must return a number representing the number of lines
+    maxLines    optional | maximun number of lines, will override the expression line count if it computes
+                to a bigger number
+]]
+function conky_bottom_edge_parse(theme, filename, x, y, voffset, expression, maxLines)
+  maxLines = tonumber(maxLines) or 1000
+  local lines = tonumber(conky_parse(expression))
+  lines = (lines > maxLines) and maxLines or lines
+  
+  return conky_bottom_edge(theme, filename, x, y, voffset, lines)
 end
 
 --[[ wrapper method for the 'bottom_edge' function
@@ -32,9 +104,10 @@ allows the client to provide a 'key' which can be used to retrieve the total num
 of the menu from a previously computed conky variable, see the method conky_compute_and_save()
 
 arguments:
+    ...         same arguments as conky_bottom_edge(..)
     key         string used to store the previously computed number of lines computation
                 see the conky_compute_and_save() function
-    maxLines    optional | maximun number of lines, will override the expression line count if it computes
+    maxLines    optional | maximum number of lines, will override the expression line count if it computes
                 to a bigger number
 ]]
 function conky_bottom_edge_load_value(theme, filename, x, y, voffset, key, maxLines)
@@ -49,51 +122,6 @@ function conky_bottom_edge_load_value(theme, filename, x, y, voffset, key, maxLi
   return conky_bottom_edge(theme, filename, x, y, voffset, lines)
 end
 
---[[ wrapper method for the 'bottom_edge' function
-allows the client to provide a conky expression/variable that when computed/parsed will return the number of lines
-in the body of the menu
-
-arguments:
-    expression  conky expression to evaluate, must return a number representing the number of lines
-    maxLines    optional | maximun number of lines, will override the expression line count if it computes
-                to a bigger number
-]]
-function conky_bottom_edge_parse(theme, filename, x, y, voffset, expression, maxLines)
-  maxLines = tonumber(maxLines) or 1000
-  local lines = tonumber(conky_parse(expression))
-  lines = (lines > maxLines) and maxLines or lines
-  
-  return conky_bottom_edge(theme, filename, x, y, voffset, lines)
-end
-
---[[ this method will print the ${image} variable required for conky to print the menu's bottom edge image based on the number of lines in the menu's body
-
-arguments:
-    theme     conky theme: compact, widets, glass, etc...
-    filename  image file name
-    x         image x coordinate
-    y         starting y coordinate of the menu's body image
-    voffset   vertical offset used for each line in the menu's body
-    lines     number of lines the body will contain
-]]
-function conky_bottom_edge(theme, filename, x, y, voffset, lines)
-  local lineMultiplier = 15
-  
-  if tonumber(voffset) > 2 then
-    lineMultiplier = lineMultiplier + tonumber(voffset) - 2
-  end
-
-  -- decrease by one line since the image's y coordinate is hard coded for the single line scenario
-  lines = (lines > 0) and (lines - 1) or 0
-  y = tonumber(y) + 14 + (lines * lineMultiplier)
-  local path = "~/conky/monochrome/images/" .. theme .. "/" .. filename
-  local s = build_image_variable(path, x, y)
-  -- add a blank image right below the bottom edge image, assume bottom edges will never be greater than 15px
-  s = s .. "${image ~/conky/monochrome/images/menu-blank.png -p " .. x .. "," .. y + 15 .. "}"
-  
-  return s
-end
-
 --[[ creates a conky image variable string, ex. ${image /directory/image.jpg -p 0,0}
 
 arguments:
@@ -105,6 +133,8 @@ function build_image_variable(path, x, y)
   return "${image " .. path .. " -p " .. x .. "," .. y .. "}"
 end
 
+-- resets the x,y offsets
+-- this method should be invoked by conky on each refresh cycle
 function conky_reset_state()
   computations["xOffset"] = 0
   computations["yOffset"] = 0
